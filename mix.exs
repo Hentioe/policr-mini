966aa7c4056a2810634d8f49298b46b0b978e8f6
Hentioe <me@bluerain.io>
2020-06-17 22:19:22 +0800

    Fix deps

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule PolicrMini.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:ecto_enum, "~> 1.4"},
       {:task_after, "~> 1.2"},
-      {:nadia, path: "../nadia"},
+      {:nadia, git: "https://github.com/Hentioe/nadia"},
       {:exi18n, "~> 0.9.1"},
       {:yaml_elixir, "~> 2.4"}
     ]
