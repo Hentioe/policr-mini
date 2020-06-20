0cdfbe7c2385a0a528ae4f4a16993ec3cfdee91a
Hentioe <me@bluerain.io>
2020-06-21 01:25:10 +0800

    Optimize the `/start` command

--- lib/policr_mini/bot/commanders/start_commander.ex
+++ lib/policr_mini/bot/commanders/start_commander.ex
@@ -63,67 +63,48 @@ defmodule PolicrMini.Bot.StartCommander do
   """
   def dispatch(arg, message), do: arg |> String.split("_") |> handle_args(message)
 
+  @spec handle_args([String.t(), ...], Nadia.Model.Message.t()) ::
+          :ok | {:error, Nadia.Model.Error.t()}
   @doc """
   处理 v1 版本的验证参数。
-  主要进行以下大致流程，按先后顺序：
-  1. 读取验证方案
-  1. 发送验证消息
-  1. 创建消息快照
-  1. 更新验证记录
   """
   def handle_args(["verification", "v1", target_chat_id], %{chat: %{id: from_user_id}} = message) do
     target_chat_id = target_chat_id |> String.to_integer()
 
     if verification = VerificationBusiness.find_unity_waiting(target_chat_id, from_user_id) do
       # 读取验证方案（当前的实现没有实际根据方案数据动态决定什么）
-      {:ok, scheme} = SchemeBusiness.fetch(target_chat_id)
-      mode = scheme.verification_mode || default!(:vmode)
+      with {:ok, scheme} <- SchemeBusiness.fetch(target_chat_id),
+           # 发送验证消息
+           {:ok, {verification_message, markup, captcha_data}} <-
+             send_verification_message(verification, scheme, target_chat_id, from_user_id),
+           # 创建消息快照
+           {:ok, message_snapshot} <-
+             MessageSnapshotBusiness.create(%{
+               chat_id: target_chat_id,
+               message_id: verification_message.message_id,
+               from_user_id: from_user_id,
+               from_user_name: fullname(message.from),
+               date: verification_message.date,
+               text: verification_message.text,
+               markup_body: Jason.encode!(markup, pretty: false)
+             }),
+           # 更新验证记录：关联消息快照、存储正确答案
+           {:ok, _} <-
+             verification
+             |> VerificationBusiness.update(%{
+               message_snapshot_id: message_snapshot.id,
+               indices: captcha_data.correct_indices
+             }) do
+      else
+        e ->
+          Logger.error(
+            "An error occurred while creating the verification message. Details: #{inspect(e)}"
+          )
 
-      captcha_module = @captchas_maping[mode] || @fallback_captcha_module
-
-      # 发送验证消息
-      captcha_data =
-        try do
-          captcha_module.make!()
-        rescue
-          e ->
-            Logger.error(
-              "An error occurred in the verification data generation of group `#{target_chat_id}`, fallback to alternatives. Details: #{
-                inspect(e)
-              }"
-            )
-
-            @fallback_captcha_module.make!()
-        end
-
-      text =
-        "请确认问题并选择您认为正确的答案。\n\n#{captcha_data.question}\n\n您还剩 #{time_left(verification)} 秒，通过可解除封印。"
-
-      markup = PolicrMini.Bot.Captcha.build_markup(captcha_data.candidates, verification.id)
-
-      {:ok, sended_verifiction_message} = send_message(from_user_id, text, reply_markup: markup)
-
-      # 创建消息快照
-      {:ok, message_snapshot} =
-        MessageSnapshotBusiness.create(%{
-          chat_id: target_chat_id,
-          message_id: sended_verifiction_message.message_id,
-          from_user_id: from_user_id,
-          from_user_name: fullname(message.from),
-          date: sended_verifiction_message.date,
-          text: sended_verifiction_message.text,
-          markup_body: Jason.encode!(markup, pretty: false)
-        })
-
-      # 更新验证记录：关联消息快照、存储正确答案
-      {:ok, _} =
-        verification
-        |> VerificationBusiness.update(%{
-          message_snapshot_id: message_snapshot.id,
-          indices: captcha_data.correct_indices
-        })
+          send_message(from_user_id, t("errors.unknown"))
+      end
     else
-      send_message(from_user_id, "您没有该目标群组的待验证记录。")
+      send_message(from_user_id, t("errors.verification_no_wating"))
     end
   end
 
@@ -133,7 +114,56 @@ defmodule PolicrMini.Bot.StartCommander do
   def handle_args(_, message) do
     %{chat: %{id: chat_id}} = message
 
-    send_message(chat_id, "很抱歉，我未能理解您的意图。")
+    send_message(chat_id, t("errors.dont_understand"))
+  end
+
+  @spec send_verification_message(
+          Verification.t(),
+          Scheme.t(),
+          integer(),
+          integer()
+        ) ::
+          {:error, Nadia.Model.Error.t()}
+          | {:ok,
+             {Nadia.Model.Message.t(), Nadia.Model.InlineKeyboardMarkup.t(), Captcha.Data.t()}}
+  @doc """
+  发送验证消息
+  """
+  def send_verification_message(verification, scheme, chat_id, user_id) do
+    mode = scheme.verification_mode || default!(:vmode)
+
+    captcha_module = @captchas_maping[mode] || @fallback_captcha_module
+
+    # 发送验证消息
+    captcha_data =
+      try do
+        captcha_module.make!()
+      rescue
+        e ->
+          Logger.error(
+            "An error occurred in the verification data generation of group `#{chat_id}`, fallback to alternatives. Details: #{
+              inspect(e)
+            }"
+          )
+
+          @fallback_captcha_module.make!()
+      end
+
+    text =
+      t("verification.template", %{
+        question: captcha_data.question,
+        seconds: time_left(verification)
+      })
+
+    markup = PolicrMini.Bot.Captcha.build_markup(captcha_data.candidates, verification.id)
+
+    case send_message(user_id, text, reply_markup: markup) do
+      {:ok, sended_message} ->
+        {:ok, {sended_message, markup, captcha_data}}
+
+      e ->
+        e
+    end
   end
 
   @doc """
