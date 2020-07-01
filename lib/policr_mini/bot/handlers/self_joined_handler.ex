84643e1e6fedc802c94e2c033f32a21e85d9bc5f
Hentioe <me@bluerain.io>
2020-07-02 05:15:04 +0800

    Fix #31

--- lib/policr_mini/bot/handlers/self_joined_handler.ex
+++ lib/policr_mini/bot/handlers/self_joined_handler.ex
@@ -12,30 +12,52 @@ defmodule PolicrMini.Bot.SelfJoinedHandler do
   单个用户加入，判断用户编号
   """
   @impl true
-  def match?(%{new_chat_members: [%{id: joined_user_id}]} = _message, state),
-    do: {joined_user_id == bot_id(), state}
+  def match?(%{new_chat_members: [%{id: member_id}]} = _message, state),
+    do: {member_id == bot_id(), state}
 
   @doc """
-  多用户加入，不匹配（目前假设拉入的用户不会和其它加入用户一起合并到一条消息中。）
+  多个用户加入，判断用户编号
   """
-  def match?(%{new_chat_members: [_, _]} = _message, state), do: {false, state}
+  def match?(%{new_chat_members: members} = _message, state),
+    do: {members |> Enum.find(fn member -> member.id == bot_id() end) != nil, state}
 
   @impl true
   def handle(message, state) do
+    chat_type = message.chat.type
+
+    if chat_type != "supergroup", do: exits(chat_type, message), else: handle_it(message)
+
+    {:ok, %{state | done: true}}
+  end
+
+  # 退出普通群。
+  defp exits("group", message) do
+    chat_id = message.chat.id
+
+    send_message(chat_id, t("errors.no_super_group"))
+    Telegex.leave_chat(chat_id)
+  end
+
+  # 退出频道。附加：目前测试被邀请进频道时并不会产生消息。
+  defp exits("channel", message) do
+    chat_id = message.chat.id
+
+    Telegex.leave_chat(chat_id)
+  end
+
+  @spec handle_it(Telegex.Message.t()) :: no_return()
+  defp handle_it(message) do
     chat_id = message.chat.id
 
     # 同步群组和管理员信息
     with {:ok, chat} <- SyncCommander.synchronize_chat(chat_id, true),
-         {:ok, _} <- SyncCommander.synchronize_administrators(chat),
-         :ok <- response(chat_id) do
-      {:ok, state}
+         {:ok, _} <- SyncCommander.synchronize_administrators(chat) do
+      :ok = response(chat_id)
     else
       e ->
         Logger.error("An error occurred after the bot was invited. Details: #{inspect(e)}")
         send_message(chat_id, t("self_joined.error"))
     end
-
-    {:ok, %{state | done: true}}
   end
 
   @spec response(integer()) :: :ok | {:error, Telegex.Model.errors()}
