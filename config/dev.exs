e5c8158c2d80d372339155d702993e6ed7618462
Hentioe <me@bluerain.io>
2020-07-14 03:10:39 +0800

    Initialize the home page

--- config/dev.exs
+++ config/dev.exs
@@ -25,11 +25,9 @@ config :policr_mini, PolicrMiniWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
+    npm: [
+      "run",
+      "watch",
       cd: Path.expand("../assets", __DIR__)
     ]
   ],
--- lib/policr_mini_web/router.ex
+++ lib/policr_mini_web/router.ex
@@ -16,27 +16,11 @@ defmodule PolicrMiniWeb.Router do
   scope "/", PolicrMiniWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", PolicrMiniWeb do
   #   pipe_through :api
   # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if PolicrMini.mix_env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: PolicrMiniWeb.Telemetry
-    end
-  end
 end
