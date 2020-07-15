1296df1d277bf4a065c0fbd3fa3628f84f86df20
Hentioe <me@bluerain.io>
2020-07-15 21:57:41 +0800

    Update page title

--- assets/src/user/components/Title.js
+++ assets/src/user/components/Title.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet";
 export default ({ children: children }) => {
   return (
     <Helmet>
-      <title>{`${children} · POLICR MINI`}</title>
+      <title>{`${children} - PolicrMini`}</title>
     </Helmet>
   );
 };
