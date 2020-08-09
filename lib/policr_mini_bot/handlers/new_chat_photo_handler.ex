4d11238abf5951f47f2762caf5042a3c3f81702b
Hentioe <me@bluerain.io>
2020-08-09 14:47:31 +0800

    Automatically sync group photos, closes #61

--- lib/policr_mini_bot/handlers/new_chat_photo_handler.ex
+++ lib/policr_mini_bot/handlers/new_chat_photo_handler.ex
@@ -5,7 +5,7 @@ defmodule PolicrMiniBot.NewChatPhotoHandler do
 
   use PolicrMiniBot, plug: :handler
 
-  # alias PolicrMini.ChatBusiness
+  alias PolicrMini.ChatBusiness
 
   @doc """
   匹配消息是否包含群组头像修改。
@@ -23,8 +23,33 @@ defmodule PolicrMiniBot.NewChatPhotoHandler do
   更新数据库中对应的群组记录的头像数据。
   """
   @impl true
-  def handle(_message, state) do
-    # TODO: 获取照片中的小图和大图，并更新相应数据
+  def handle(message, state) do
+    %{chat: %{id: chat_id}} = message
+
+    # 获取照片中的小图和大图，并更新相应数据
+    case ChatBusiness.get(chat_id) do
+      {:ok, chat} ->
+        ChatBusiness.update(chat, %{
+          small_photo_id: small_photo_id(message),
+          big_photo_id: big_photo_id(message)
+        })
+
+        {:ok, state}
+
+      _ ->
+        {:ok, state}
+    end
+
     {:ok, state}
   end
+
+  # 从消息中获取小尺寸头像的文件 id
+  @spec small_photo_id(Message.t()) :: String.t()
+  defp small_photo_id(%{new_chat_photo: []}), do: nil
+  defp small_photo_id(%{new_chat_photo: [photo | _]}), do: photo.file_id
+
+  # 从消息中获取大尺寸头像的文件 id
+  @spec big_photo_id(Message.t()) :: String.t()
+  defp big_photo_id(%{new_chat_photo: []}), do: nil
+  defp big_photo_id(%{new_chat_photo: [_, _, photo]}), do: photo.file_id
 end
