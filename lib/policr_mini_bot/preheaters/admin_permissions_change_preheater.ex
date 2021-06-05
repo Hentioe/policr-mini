7dd7bbebeac6a923a2f983f41f5605692ce65c69
Hentioe <me@bluerain.io>
2021-06-05 10:28:28 +0800

    Ignore permission changes for bot members

--- lib/policr_mini_bot/preheaters/admin_permissions_change_preheater.ex
+++ lib/policr_mini_bot/preheaters/admin_permissions_change_preheater.ex
@@ -15,6 +15,7 @@ defmodule PolicrMiniBot.AdminPermissionsChangePreheater do
   - 更新不包含 `chat_member` 数据。
   - 更新来自频道。
   - 状态中的 `action` 字段为 `:user_joined` 或 `:user_lefted`。备注：用户加入和管理员权限无关，用户离开有独立的模块处理权限。
+  - 成员的用户类型是机器人。
   - 成员现在的状态是 `member` 或 `restricted`，并且之前的状态也是 `memeber`、`restricted`。备注：权限变化和管理员权限无关。
   - 成员现在的状态是 `left` 并且之前的状态是 `kicked` 或 `restricted`。备注：从封禁或例外列表中解封用户和管理员权限变化无关。
   - 成员现在的状态是 `restricted` 并且之前的状态是 `left` 或 `kicked`。备注：将不在群内的用户添加到例外或封禁列表中和管理员权限变化无关。
@@ -35,6 +36,11 @@ defmodule PolicrMiniBot.AdminPermissionsChangePreheater do
 
   @impl true
   def call(_update, %{action: action} = state) when action in [:user_joined, :user_lefted] do
+    {:ignored, state}
+  end
+
+  def call(%{chat_member: %{new_chat_member: %{user: %{is_bot: is_bot}}}} = _update, state)
+      when is_bot == true do
     {:ignored, state}
   end
 
@@ -108,7 +114,7 @@ defmodule PolicrMiniBot.AdminPermissionsChangePreheater do
     with {:ok, chat} <- ChatBusiness.get(chat_id),
          {:ok, _} <- SyncCommander.synchronize_administrators(chat) do
       text = """
-      检测到用户 #{mention(user, anonymization: false, parse_mode: "HTML")} 的管理员权限变化，已自动同步至最新的后台权限中。
+      检测到用户 #{mention(user, anonymization: false, parse_mode: "HTML")} 的管理权限变化，已自动同步至后台权限中。
 
       <i>提示：由于此特性的加入，在管理员权限变化的场景下将不再需要手动调用 <code>/sync</code> 命令。</i>
       """
@@ -118,7 +124,7 @@ defmodule PolicrMiniBot.AdminPermissionsChangePreheater do
       e ->
         send_message(
           chat_id,
-          "检测到用户 #{mention(user, anonymization: false)} 的管理员权限变化，但由于某些原因自动同步后台权限失败了。"
+          "检测到用户 #{mention(user, anonymization: false)} 的管理权限变化，但由于某些原因同步到后台权限失败了。"
         )
 
         Logger.unitized_error("Automatically sync administrator permissions",
