0096e2a03119d99b6bfe01cf3455340efcf906e3
Hentioe <me@bluerain.io>
2021-07-03 16:10:32 +0800

    Drop some fromt fields of sponsor

--- lib/policr_mini_web/views/api/sponsor_view.ex
+++ lib/policr_mini_web/views/api/sponsor_view.ex
@@ -7,6 +7,8 @@ defmodule PolicrMiniWeb.API.SponsorView do
 
   @spec render(String.t(), map()) :: map()
   def render("sponsor.json", %{sponsor: sponsor}) do
-    sponsor |> Map.drop([:__meta__, :uuid, :inserted_at, :updated_at]) |> Map.from_struct()
+    sponsor
+    |> Map.drop([:__meta__, :uuid, :contact, :inserted_at, :updated_at])
+    |> Map.from_struct()
   end
 end
