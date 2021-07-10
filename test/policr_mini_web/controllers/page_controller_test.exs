5daef8cf5e0826c03ae151ee92c28bbbb97c3c3d
Hentioe <me@bluerain.io>
2021-07-10 11:23:35 +0800

    Fix test errors

--- test/policr_mini_web/controllers/page_controller_test.exs
+++ test/policr_mini_web/controllers/page_controller_test.exs
@@ -1,8 +1,12 @@
 defmodule PolicrMiniWeb.PageControllerTest do
   use PolicrMiniWeb.ConnCase
 
+  alias PolicrMiniBot.UpdatesPoller.BotInfo
+
   # TODO：此处初始化  :ets 中的 `PolicrMiniBot.UpdatesPoller.BotInfo` 表的模拟数据。
   test "GET /", %{conn: conn} do
+    :ets.new(BotInfo, [:set, :named_table])
+
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "载入中 - "
   end
