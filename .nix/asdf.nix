489ffd84a4ec112a33d42dc9363f4ac8deb8f2f3
Hentioe <me@bluerain.io>
2021-07-14 18:18:33 +0800

    Update asdf.nix

--- .nix/asdf.nix
+++ .nix/asdf.nix
@@ -1,29 +1,31 @@
-# TODO: 解决和 wx 相关的构建错误。
-# 当前通过此环境所构建的 erlang 无法启动 observer 等模块的图形功能。
-
 with import <nixpkgs> { };
 
 stdenv.mkDerivation {
   name = "asdf-erlang-build-environment";
+
   buildInputs = [
     pkg-config
     gnumake
-    autoconf269
+    autoconf
     ncurses
     openssl
     libxslt
     fop
     libxml2
-    openjdk11
-    unixODBC
     libGL
     libGLU
-    wxGTK
+    # 从处不能使用 wxGTK31，具体细节和 wxGTK 包的默认版本有关。
+    # 详情：https://github.com/NixOS/nixpkgs/issues/63579
+    (wxGTK30.override {
+      withWebKit = true;
+      withGtk2 = false;
+    })
     xorg.libX11
   ];
 
   KERL_CONFIGURE_OPTIONS =
     "--with-ssl=${lib.getOutput "out" openssl} --with-ssl-incl=${
       lib.getDev openssl
-    } --with-odbc=${unixODBC} --enable-wx";
+    } --enable-jit --without-javac --without-odbc";
+
 }
