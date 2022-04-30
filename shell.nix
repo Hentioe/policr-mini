d3aa8827af536f58ac1522a685e0b302bbe47188
Hentioe <meow.i5.br@gmail.com>
2022-05-01 06:55:36 +0800

    Enable multi time warp mode

--- shell.nix
+++ shell.nix
@@ -1,3 +1,8 @@
 with import <nixpkgs> { };
 
-mkShell { buildInputs = [ python3 inotify-tools ]; }
+mkShell {
+  buildInputs = [ python3 inotify-tools ];
+  shellHook = ''
+    export ERL_AFLAGS="+C multi_time_warp";
+  '';
+}
