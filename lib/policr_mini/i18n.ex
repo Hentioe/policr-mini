30aaa6fa52a2264d18cbb77e4e7270eb98db7d2f
Hentioe <meow.i5.br@gmail.com>
2023-05-11 04:14:03 +0800

    Add i18n module

---
+++ lib/policr_mini/i18n.ex
@@ -0,0 +1,27 @@
+defmodule PolicrMini.I18n do
+  @moduledoc false
+
+  defmacro __using__(_) do
+    quote do
+      import PolicrMiniWeb.Gettext
+      import unquote(__MODULE__)
+
+      require unquote(__MODULE__)
+    end
+  end
+
+  defmacro commands_text(msg_id, bindings \\ []) do
+    msg_id =
+      cond do
+        is_binary(msg_id) ->
+          String.trim(msg_id)
+
+        true ->
+          msg_id
+      end
+
+    quote do
+      dgettext("commands", unquote(msg_id), unquote(bindings))
+    end
+  end
+end
