cf44df6732d02f14d02f2e0b61f6257e097994e3
Hentioe <meow.i5.br@gmail.com>
2023-05-14 02:41:51 +0800

    Internationalized login command text

--- lib/policr_mini_bot/plugs/resp_login_cmd_plug.ex
+++ lib/policr_mini_bot/plugs/resp_login_cmd_plug.ex
@@ -10,6 +10,10 @@ defmodule PolicrMiniBot.RespLoginCmdPlug do
 
   require Logger
 
+  @sync_comment_text commands_text(
+                       "如果您确定自己是群管理员，且群组中的确使用了本机器人。请通知群主或其它管理员在群内使用 `/sync` 命令同步最新数据。"
+                     )
+
   @doc """
   处理登录命令。
 
@@ -20,10 +24,29 @@ defmodule PolicrMiniBot.RespLoginCmdPlug do
 
     with {:ok, :isadmin} <- check_user(user_id),
          {:ok, token} <- PolicrMiniWeb.create_token(user_id) do
-      text = t("login.success", %{token: token})
+      theader = commands_text("已为您创建一枚令牌，点击下方按钮可直接进入后台。亦或复制令牌手动登入。")
+
+      tfooter =
+        commands_text("有效期为 %{duration} %{unit}，过期需重新申请和登入。如怀疑泄漏请立即吊销。",
+          duration: "1",
+          unit: gettext("天")
+        )
+
+      tcomment = commands_text("安全小贴士：不可将按钮中的链接或登录令牌分享于他人，除非您想将控制权短暂的共享于他。")
+
+      text = """
+      #{theader}
+
+      <code>#{token}</code>
+
+      #{tfooter}
+
+      <i>#{tcomment}</i>
+      """
+
       reply_markup = make_markup(user_id, token)
 
-      case send_message(chat_id, text, reply_markup: reply_markup, parse_mode: "MarkdownV2ToHTML") do
+      case send_message(chat_id, text, reply_markup: reply_markup, parse_mode: "HTML") do
         {:ok, _} ->
           {:ok, state}
 
@@ -34,12 +57,32 @@ defmodule PolicrMiniBot.RespLoginCmdPlug do
       end
     else
       {:error, :nonadmin} ->
-        send_message(chat_id, t("login.non_admin"), parse_mode: "MarkdownV2ToHTML")
+        theader = commands_text("未找到和您相关的权限记录")
+
+        tcomment = @sync_comment_text
+
+        text = """
+        <b>#{theader}</b>
+
+        <i>#{tcomment}</i>
+        """
+
+        send_message(chat_id, text, parse_mode: "HTML")
 
         {:ok, state}
 
       {:error, :notfound} ->
-        send_message(chat_id, t("login.not_found"), parse_mode: "MarkdownV2ToHTML")
+        theader = commands_text("未找到和您相关的用户记录")
+
+        tcomment = @sync_comment_text
+
+        text = """
+        <b>#{theader}</b>
+
+        <i>#{tcomment}</i>
+        """
+
+        send_message(chat_id, text, parse_mode: "HTML")
 
         {:ok, state}
     end
@@ -47,7 +90,7 @@ defmodule PolicrMiniBot.RespLoginCmdPlug do
 
   def handle(message, state) do
     %{chat: %{id: chat_id}, message_id: message_id} = message
-    text = t("login.only_private")
+    text = commands_text("请在私聊中使用此命令。")
 
     case reply_message(chat_id, message_id, text) do
       {:ok, sended_message} ->
@@ -70,13 +113,13 @@ defmodule PolicrMiniBot.RespLoginCmdPlug do
       inline_keyboard: [
         [
           %InlineKeyboardButton{
-            text: t("login.revoke_text"),
+            text: commands_text("吊销全部令牌"),
             callback_data: "revoke:v1:#{user_id}"
           }
         ],
         [
           %InlineKeyboardButton{
-            text: t("login.join_text"),
+            text: commands_text("进入后台"),
             url: "#{root_url}/admin?token=#{token}"
           }
         ]
