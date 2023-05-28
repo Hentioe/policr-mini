2a0e5b54766fb13ca8ea2580dd1992ea46c739e6
Hentioe <meow.i5.br@gmail.com>
2023-05-28 21:58:31 +0800

    decline join request after verification expires

--- lib/policr_mini_bot/runner/expired_checker.ex
+++ lib/policr_mini_bot/runner/expired_checker.ex
@@ -16,34 +16,42 @@ defmodule PolicrMiniBot.Runner.ExpiredChecker do
   """
   def run do
     # 获取所有处于等待状态的验证
-    verifications = Chats.find_all_pending_verifications()
-    # 计算已经过期的验证
-    verifications =
-      Enum.filter(verifications, fn v ->
-        remaining_seconds = DateTime.diff(DateTime.utc_now(), v.inserted_at)
-        remaining_seconds - (v.seconds + 30) > 0
-      end)
+    vs = Chats.find_all_pending_verifications()
+    # 计算已经过期的验证列表
+    vs = Enum.filter(vs, &expired?/1)
 
     # 修正状态
     # TODO: 待优化：在同一个事物中更新所有验证记录
-    Enum.each(verifications, fn verification ->
-      # 自增统计数据（其它）。
-      async do
-        Chats.increment_statistic(
-          verification.chat_id,
-          verification.target_user_language_code,
-          :other
-        )
-      end
+    Enum.each(vs, fn v ->
+      # 自增统计数据（其它）
+      async_run(fn ->
+        Chats.increment_statistic(v.chat_id, v.target_user_language_code, :other)
+      end)
 
-      Chats.update_verification(verification, %{status: :expired})
+      # 更新为过期状态
+      Chats.update_verification(v, %{status: :expired})
+      # 处理验证用户
+      user_processing(v)
     end)
 
-    log_auto_corrected(verifications)
+    auto_corrected_logging(vs)
   end
 
-  @spec log_auto_corrected([Verification.t()]) :: :ok | :ignore
-  defp log_auto_corrected([v]) do
+  @spec user_processing(Verification.t()) :: :ok
+  # 来源为 `:join_request` 的验证，拒绝加入请求
+  defp user_processing(%{source: :join_request} = v) do
+    async_run(fn -> Telegex.decline_chat_join_request(v.chat_id, v.target_user_id) end)
+
+    :ok
+  end
+
+  # 来源为 `:joined` 的验证，忽略处理（因为权限已被限制）
+  defp user_processing(%{source: :joined} = _v) do
+    :ok
+  end
+
+  @spec auto_corrected_logging([Verification.t()]) :: :ok | :ignore
+  defp auto_corrected_logging([v]) do
     Logger.warning(
       "Auto-corrected a verification: #{inspect(id: v.id, user_id: v.target_user_id)}",
       chat_id: v.chat_id
@@ -52,13 +60,18 @@ defmodule PolicrMiniBot.Runner.ExpiredChecker do
     :ok
   end
 
-  defp log_auto_corrected([]) do
+  defp auto_corrected_logging([]) do
     :ignore
   end
 
-  defp log_auto_corrected(verifications) do
-    Logger.warning("Auto-corrected #{length(verifications)} verifications")
+  defp auto_corrected_logging(vs) do
+    Logger.warning("Auto-corrected #{length(vs)} verifications")
 
     :ok
+  end
+
+  defp expired?(v) do
+    remaining_seconds = DateTime.diff(DateTime.utc_now(), v.inserted_at)
+    remaining_seconds - (v.seconds + 30) > 0
   end
 end
