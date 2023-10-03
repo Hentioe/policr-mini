92536d9fafce083839ac58770830591f181d2ae0
Hentioe <meow.i5.br@gmail.com>
2023-10-04 00:22:30 +0800

    Add todos

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule PolicrMini.MixProject do
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:telegex, "~> 1.2.1"},
+      # TODO: 适配 Telegex 1.x 后移除。
       {:telegex_plug, "~> 0.3"},
       {:phoenix, "~> 1.5"},
       {:phoenix_ecto, "~> 4.2"},
