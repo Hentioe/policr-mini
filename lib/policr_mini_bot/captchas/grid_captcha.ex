322bbd7ef5ea9f7aee54430758456af220983ccb
Hentioe <meow.i5.br@gmail.com>
2023-11-20 07:23:00 +0800

    Improve grid CAPTCHA

--- lib/policr_mini_bot/captchas/grid_captcha.ex
+++ lib/policr_mini_bot/captchas/grid_captcha.ex
@@ -11,7 +11,11 @@ defmodule PolicrMiniBot.GridCAPTCHA do
     defexception [:message]
   end
 
-  @errmsg "There are not enough albums, please try to increase the number of albums or reduce their reference relationship"
+  defp raise_error!(message) do
+    raise Error, message
+  end
+
+  @font_family "FreeMono"
   @grid_size 9
   @expected_similar_count 3
 
@@ -20,7 +24,10 @@ defmodule PolicrMiniBot.GridCAPTCHA do
     # 获取图集列表（等于网格大小）。
     albums = ImageProvider.random_albums(@grid_size)
     # 检查图集数量是否充足。
-    if length(albums) < @grid_size, do: raise(Error, @errmsg)
+    if length(albums) < @grid_size do
+      raise_error!("Insufficient number of albums, please upload more albums")
+    end
+
     # 生成一个作为正确答案的图集。
     corrent_album_pos = Enum.random(0..(@grid_size - 1))
     correct_album = Enum.at(albums, corrent_album_pos)
@@ -73,14 +80,14 @@ defmodule PolicrMiniBot.GridCAPTCHA do
       end)
 
     # 正确的候选答案。
-    correct_candidate = Enum.map_join(correct_pairs, "", fn {number, _image} -> number end)
+    corcand = Enum.map_join(correct_pairs, "", fn {number, _image} -> number end)
     # 生成一个随机的正确答案位置。
-    correct_index = Enum.random(1..9)
+    coridx = Enum.random(1..9)
     # 生成全部候选答案（正确的候选答案将插入到指定位置）。
     candidates =
-      correct_candidate
-      |> generate_incorrect_candidates()
-      |> List.insert_at(correct_index - 1, correct_candidate)
+      corcand
+      |> gen_incorcands()
+      |> List.insert_at(coridx - 1, corcand)
 
     # 生成网格图片。
     {:ok, photo} = generate_image(Enum.map(container, fn image -> image.path end))
@@ -89,29 +96,30 @@ defmodule PolicrMiniBot.GridCAPTCHA do
       question: "选出所有「#{correct_album.name.zh_hans}」的图片编号",
       photo: photo,
       candidates: Enum.chunk_every(candidates, 3),
-      correct_indices: [correct_index]
+      correct_indices: [coridx]
     }
   end
 
-  @spec generate_incorrect_candidates(String.t(), [String.t()], non_neg_integer()) :: [String.t()]
-  defp generate_incorrect_candidates(correct_candidate, incorrect_candidates \\ [], count \\ 0),
-    do: _generate_incorrect_candidates(correct_candidate, incorrect_candidates, count)
-
-  # 生成数量达到 8 个，直接返回。
-  defp _generate_incorrect_candidates(_correct_candidate, incorrect_candidates, _count = 8) do
-    incorrect_candidates
+  @spec gen_incorcands(String.t(), [String.t()], non_neg_integer) :: [String.t()]
+  defp gen_incorcands(corcand, incorcands \\ [], count \\ 0) do
+    _gen_incorcands(corcand, incorcands, count)
   end
 
-  defp _generate_incorrect_candidates(correct_candidate, incorrect_candidates, count) do
-    ic = 1..9 |> Enum.shuffle() |> Enum.take(3) |> Enum.join()
+  # 生成数量达到 8 个，直接返回。
+  defp _gen_incorcands(_corcand, incorcands, _count = 8) do
+    incorcands
+  end
+
+  defp _gen_incorcands(corcand, incorcands, count) do
+    incorcand = 1..9 |> Enum.shuffle() |> Enum.take(3) |> Enum.join()
 
     cond do
-      ic != correct_candidate ->
-        _generate_incorrect_candidates(correct_candidate, [ic | incorrect_candidates], count + 1)
+      incorcand != corcand ->
+        _gen_incorcands(corcand, [incorcand | incorcands], count + 1)
 
       true ->
         # 如果和正确答案重复，重新生成。
-        _generate_incorrect_candidates(correct_candidate, incorrect_candidates, count)
+        _gen_incorcands(corcand, incorcands, count)
     end
   end
 
@@ -126,7 +134,7 @@ defmodule PolicrMiniBot.GridCAPTCHA do
       target_dir: target_dir,
       indi_width: config_get(:indi_width, 180),
       indi_height: config_get(:indi_height, 120),
-      watermark_font_family: config_get(:watermark_font_family, "FreeMono")
+      watermark_font_family: config_get(:watermark_font_family, @font_family)
     }
 
     ImgGrider.generate(photos, scheme)
