381f78072e0982d12c76110c3a122742d61ce463
Hentioe <meow.i5.br@gmail.com>
2024-01-04 08:05:28 +0800

    Upgrade `telegex`

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule PolicrMini.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:rustler, "~> 0.30.0", runtime: false},
       {:benchee, "~> 1.0", only: :dev},
-      {:telegex, "~> 1.3.2"},
+      {:telegex, "~> 1.4.0"},
       {:phoenix, "~> 1.5"},
       {:phoenix_ecto, "~> 4.2"},
       {:postgrex, "~> 0.17"},
