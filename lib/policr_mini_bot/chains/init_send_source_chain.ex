fcebcbfea42e2e1825dc9c7678ebb9bb6b505cef
Hentioe <meow.i5.br@gmail.com>
2024-01-04 08:17:03 +0800

    Ignore channel send source

--- lib/policr_mini_bot/chains/init_send_source_chain.ex
+++ lib/policr_mini_bot/chains/init_send_source_chain.ex
@@ -5,6 +5,14 @@ defmodule PolicrMiniBot.InitSendSourceChain do
 
   import PolicrMiniBot.Helper.FromParser
 
+  # 来自频道的新消息。
+  @impl true
+  def handle(%{channel_post: channel_post} = _update, context) when channel_post != nil do
+    # 直接忽略，不继续传播链。
+
+    {:stop, context}
+  end
+
   @impl true
   def handle(update, context) do
     case parse(update) do
