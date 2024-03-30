c57e4701992c9a9e25849d836775d7930336a4f0
Hentioe <meow.i5.br@gmail.com>
2024-03-31 07:01:13 +0800

    Fix prod assets path

--- lib/policr_mini_web/endpoint.ex
+++ lib/policr_mini_web/endpoint.ex
@@ -27,7 +27,7 @@ defmodule PolicrMiniWeb.Endpoint do
     at: "/",
     from: :policr_mini,
     gzip: false,
-    only: ~w(css fonts images svg js favicon.ico robots.txt)
+    only: ~w(assets css fonts images svg js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
