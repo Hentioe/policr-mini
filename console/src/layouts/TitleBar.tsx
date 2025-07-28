eac57d12fa1e8fbc8fe867e78e4092d049e96fc5
Hentioe <meow.i5.br@gmail.com>
2025-07-28 11:45:55 +0800

    Increase the click range of the hamburger button

--- console/src/layouts/TitleBar.tsx
+++ console/src/layouts/TitleBar.tsx
@@ -12,8 +12,10 @@ export default () => {
   };
 
   return (
-    <header class="fixed top-0 w-full h-header bg-white/40 backdrop-blur-md shadow flex items-center justify-between px-edge">
-      <Icon onClick={handleToggleDrawer} icon="mdi:menu" class="text-[2rem] w-[2rem]" />
+    <header class="fixed top-0 w-full h-header bg-white/40 backdrop-blur-md shadow flex items-center justify-between pr-edge">
+      <div onClick={handleToggleDrawer} class="px-edge h-full flex items-center">
+        <Icon icon="mdi:menu" class="text-[2rem] w-[2rem]" />
+      </div>
       <h1 class="text-lg font-bold text-center px-[1rem] line-clamp-1">
         {title()}
         {currentChatTitle() && ` / ${currentChatTitle()}`}
