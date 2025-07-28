e59853b65c8ff5a6b91726350ea3e91d05fc5c8c
Hentioe <meow.i5.br@gmail.com>
2025-07-28 23:19:14 +0800

    Add background to console user card

--- console/src/layouts/drawer/User.tsx
+++ console/src/layouts/drawer/User.tsx
@@ -4,9 +4,9 @@ import Loading from "../../components/Loading";
 {/* todo: 颜色不断运动的彩色背景 */}
 export default (props: { data?: ServerData.User }) => {
   return (
-    <div class="p-[1rem] h-[12.5rem] bg-white/40 backdrop-blur-md">
+    <div class="p-[1rem] h-[13rem] bg-white/40 backdrop-blur-md">
       <Show when={props.data} fallback={<MyLoading />}>
-        <div class="w-full py-[1rem] bg-zinc-300 rounded-xl shadow flex flex-col items-center gap-[0.5rem]">
+        <div class="w-full py-[1rem] user-bg rounded-xl shadow flex flex-col items-center gap-[0.5rem]">
           <img
             width={320}
             height={320}
@@ -14,9 +14,11 @@ export default (props: { data?: ServerData.User }) => {
             alt="用户头像"
             class="w-[7rem] h-[7rem] rounded-full shadow"
           />
-          <p class="h-[1rem] text-gray-600 text-center">
-            你好，{props.data?.fullName}
-          </p>
+          <div class="h-[1.5rem] flex items-center px-[1rem] text-zinc-50 bg-zinc-300/30 rounded-full max-w-user-prompt">
+            <p class="text-nowrap line-clamp-1 truncate">
+              你好，{props.data?.fullName}
+            </p>
+          </div>
         </div>
       </Show>
     </div>
