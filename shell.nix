320e597a7b02e5787da7d4bb0252070dfdb01863
Hentioe <meow.i5.br@gmail.com>
2025-07-30 02:29:49 +0800

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,8 @@
 with import <nixpkgs> { };
 
 mkShell {
-  buildInputs = [ python3 inotify-tools nodejs_22 pkg-config imagemagick lato ];
+  buildInputs = [ python3 inotify-tools nodejs_22 ];
   shellHook = ''
     export ERL_AFLAGS="+C multi_time_warp";
   '';
-
-  LIBCLANG_PATH =
-    pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
 }
