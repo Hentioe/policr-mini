0e41dc8940b929e44aca84fbdf7c7aac2e784d1d
Hentioe <meow.i5.br@gmail.com>
2025-07-30 05:14:13 +0800

    The console drawer is opened by default in production builds

--- console/src/state/global.ts
+++ console/src/state/global.ts
@@ -12,7 +12,7 @@ const DEFAULT_PAGE: Page = "stats";
 
 const [store, setStore] = createStore<State>({
   currentPage: DEFAULT_PAGE,
-  drawerIsOpen: false,
+  drawerIsOpen: import.meta.env.MODE === "production",
   currentChatId: null,
   currentChatTitle: null,
   emptyChatList: null,
